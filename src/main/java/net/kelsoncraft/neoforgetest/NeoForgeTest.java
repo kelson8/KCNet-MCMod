d5cdfb47b82c5d90f0c42ae9bb5cad393030ad9f
kelson8 <kelson@kelsoncraft.net>
2025-07-16 16:19:55 -0400

    Add bismuth as a test item * Add MIT license

--- src/main/java/net/kelsoncraft/neoforgetest/NeoForgeTest.java
+++ src/main/java/net/kelsoncraft/neoforgetest/NeoForgeTest.java
@@ -1,5 +1,7 @@
 package net.kelsoncraft.neoforgetest;
 
+import net.kelsoncraft.neoforgetest.item.ModItems;
+import net.minecraft.world.item.CreativeModeTabs;
 import org.slf4j.Logger;
 
 import com.mojang.logging.LogUtils;
@@ -25,18 +27,32 @@ public class NeoForgeTest {
     // The constructor for the mod class is the first code that is run when your mod is loaded.
     // FML will recognize some parameter types like IEventBus or ModContainer and pass them in automatically.
     public NeoForgeTest(IEventBus modEventBus, ModContainer modContainer) {
+        //---
         // Register the commonSetup method for modloading
         modEventBus.addListener(this::commonSetup);
+        //---
 
+        //---
         // Register ourselves for server and other game events we are interested in.
         // Note that this is necessary if and only if we want *this* class (ExampleMod) to respond directly to events.
         // Do not add this line if there are no @SubscribeEvent-annotated functions in this class, like onServerStarting() below.
+        //---
         NeoForge.EVENT_BUS.register(this);
 
+        //---
+        // Register items
+        //---
+
+        ModItems.register(modEventBus);
+
+        //---
         // Register the item to a creative tab
+        //---
         modEventBus.addListener(this::addCreative);
 
+        //---
         // Register our mod's ModConfigSpec so that FML can create and load the config file for us
+        //---
         modContainer.registerConfig(ModConfig.Type.COMMON, Config.SPEC);
     }
 
@@ -47,6 +63,10 @@ public class NeoForgeTest {
 
     // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event) {
+        if(event.getTabKey() == CreativeModeTabs.INGREDIENTS) {
+            event.accept(ModItems.BISMUTH);
+            event.accept(ModItems.RAW_BISMUTH);
+        }
     }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
